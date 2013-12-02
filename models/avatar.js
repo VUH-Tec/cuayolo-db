95b8263fd113f88a35859d04efef63a3f3861a15
VatslavDS <gardna_big@hotmail.com>
2013-12-02 13:28:22 +0000

    Actualizando avatar...

--- models/avatar.js
+++ models/avatar.js
@@ -1,28 +1,35 @@
 //fix
-var Avatar = new Schema({
-    id_avatar : Number,
-    url_avatar : String,
-    alumnos_id_alumnos : Number,
-    piel : [Piel],
-    cabello : [Cabello]
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+function AvatarDAO(){
+    var Avatar = new Schema({
+	id_avatar : Number,
+	url_avatar : String,
+	alumnos_id_alumnos : Number,
+	piel : [Piel],
+	cabello : [Cabello]
     });
 
-var Cabello = new Schema({
-    id_cabello : Number,
-    url_cabello : String
+    var Cabello = new Schema({
+	id_cabello : Number,
+	url_cabello : String
     });
 
-var Piel = new Schema({
-    id_piel : Number,
-    url_piel : String
+    var Piel = new Schema({
+	id_piel : Number,
+	url_piel : String
     });
 
-/*
-  db.avatar.insert({
-  id_avatar : 0, url_avatar : '/src/photo.jpg', alumno_id : 0, piel : [{id_piel : 0, url_piel : '/src/piel.jpg'}], cabello : [{id_cabello : 0, url_cabello : '/src/cabello.jpg'}]
-  });
-  Ejemplos de queries: encontrar avatar donde id_avatar sea mayor o igual a 1
-  db.avatar.find({id_avatar : {$gte : 1}});
-  Ejemplos de queries: encontrar avatar donde alumno_id sea mayor o igual a 10 y no projecta el id_avatar
-  db.avatar.find({alumno_id : {$gte : 10}}, {id_avatar : 0});
-  */
+    /*
+      db.avatar.insert({
+      id_avatar : 0, url_avatar : '/src/photo.jpg', alumno_id : 0, piel : [{id_piel : 0, url_piel : '/src/piel.jpg'}], cabello : [{id_cabello : 0, url_cabello : '/src/cabello.jpg'}]
+      });
+      Ejemplos de queries: encontrar avatar donde id_avatar sea mayor o igual a 1
+      db.avatar.find({id_avatar : {$gte : 1}});
+      Ejemplos de queries: encontrar avatar donde alumno_id sea mayor o igual a 10 y no projecta el id_avatar
+      db.avatar.find({alumno_id : {$gte : 10}}, {id_avatar : 0});
+    */
+}
+
+module.exports.AvatarDAO = AvatarDAO;
