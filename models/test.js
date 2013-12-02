5d13f7dc0fa9946f7db3cc3190864528e22415fa
VatslavDS <gardna_big@hotmail.com>
2013-12-02 15:50:57 +0000

    TestDAO

---
+++ models/test.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+function Test(){
+    this.Test = new Scehma({
+	id_alumno : Number,
+	/*
+	  //////////////////////////////////
+
+	  Que otros datos ser
+
+	  //////////////////////////////////
+	  */
+	});
+}
+
+module.exports.Test = Test;
