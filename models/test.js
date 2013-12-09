b783d160292aa7d4c93e53a65c559d09e2aee839
VatslavDS <gardna_big@hotmail.com>
2013-12-09 17:33:13 +0000

    Un beta de los test

--- models/test.js
+++ models/test.js
@@ -1,17 +1,26 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-function Test(){
-    this.Test = new Scehma({
-	id_alumno : Number,
-	/*
-	  //////////////////////////////////
+function TestDAO(){
+    this.test = new Schema({
+	id_test : Number,
+	nombre_test : String,
+	preguntas_test : [], //Mapeo a preguntasTest Object
+	respuestas_test : [] //Mapeo a respuestasTest Object
+	});
 
-	  Que otros datos ser
+    this.preguntaTest = new Schema({
+	id_pregunta : Number,
+	id_respuestaTest : Number,
+	pregunta : String
+	});
 
-	  //////////////////////////////////
-	  */
+    this.respuestaTest = new Schema({
+	id_respuesta : Number,
+	id_preguntaTest : Number,
+	respuestas : [], //Todas las posibles respuestas vienen aqui
+	respuesta_correcta : String
 	});
 }
 
-module.exports.Test = Test;
+module.exports = TestDAO;
